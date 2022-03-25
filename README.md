8b56e66eacf7cbc9fb483de40cd92f6ee832b536
shaddy tosh <shadrackrito@gmail.com>
2022-03-25 13:05:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Overview
+QrnAnd Barcode Scanner App
---
+++ app/src/main/java/com/example/barcodescanner/model/schema/Schema.kt
@@ -0,0 +1,28 @@
+package com.example.barcodescanner.model.schema
+
+enum class BarcodeSchema {
+    APP,
+    BOOKMARK,
+    CRYPTOCURRENCY,
+    EMAIL,
+    GEO,
+    GOOGLE_MAPS,
+    MMS,
+    MECARD,
+    OTP_AUTH,
+    PHONE,
+    SMS,
+    URL,
+    VEVENT,
+    VCARD,
+    WIFI,
+    YOUTUBE,
+    NZCOVIDTRACER,
+    OTHER;
+}
+
+interface Schema {
+    val schema: BarcodeSchema
+    fun toFormattedText(): String
+    fun toBarcodeText(): String
+}
